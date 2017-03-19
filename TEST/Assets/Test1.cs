33ebdb700942cc3063ce11b3cf2a587ca5ae9f61
VasilOrlinov <vasil.orlinov@gmail.com>
2017-03-19 16:25:44 +0200

    GOgo

---
+++ TEST/Assets/Test1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test1: MonoBehaviour
+{
+    public GameObject _object;
+    public GameObject _player;
+       
+    void Start()
+    {
+        
+    }
+
+   void Update()
+    {
+        transform.LookAt(_player.transform);
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
